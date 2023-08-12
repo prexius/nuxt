a567ec1ca179b96c57372b1e08476fa2a3056167
imsaifun <imsaifun@gmail.com>
2023-08-12 20:55:26 +0600

    layout done

---
+++ components/ProductCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="card text-center">
+        <img :src="product.image" alt="" class="thumb" />
+        <NuxtLink :to="`/products/${product.id}`">
+            <p class="font-bold">{{ product.title }}</p>
+        </NuxtLink>
+    </div>
+</template>
+
+<script setup>
+const { product } = defineProps(['product'])
+
+</script>
+
+<style scoped></style>
---
+++ error.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h3>{{ error.message }}</h3>
+    </div>
+</template>
+
+<script setup>
+defineProps(['error'])
+</script>
+
+<style scoped></style>
